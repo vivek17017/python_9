2f93a94ac3794154094e86c47d576e7eb9a9ef39
Vivek Kumar <vivekgupta70509@gmail.com>
2025-04-06 18:22:37 +0530

    Add files via upload

---
+++ Assignment9.py
@@ -0,0 +1,35 @@
+"""
+#Q1
+p=100
+r=5
+t=4
+si=(p*r*t)/100
+print(si)
+
+#Q2
+l=5
+b=4
+print("area is ",l*b)
+
+
+
+#Q3
+a=int(input("Enter first no "))
+b=int(input("Enter second no"))
+c=int(input("Enter third no"))
+avg=(a+b+c)/3
+print(avg)
+"""
+
+#Q4
+l,b,h=3,5,6
+print("vol is ",l*b*h)
+
+#Q5
+x=int(input("Enter first no "))
+y=int(input("Enter second no"))
+b=x*y
+print(b)
+
+
+
